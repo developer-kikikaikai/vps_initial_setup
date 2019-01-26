366f8f6f12449052e53236bb7947037232ef8025
developer-kikikaikai <kikikaikaikai@gmail.com>
2019-01-26 11:40:05 +0900

    Add mysql install

--- extra_pkg.sh
+++ extra_pkg.sh
@@ -1,3 +1,9 @@
+#install for golang 
 yum install -y epel-release 
-yum install -y mysql golang python36 sl
+yum install -y golang python36 sl
+
+#install DB server
+#curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+yum --disablerepo=mariadb-main install mariadb-server
+
 sl
