b1b9d8d30b8490f1bb49bfcc2926bc48348768eb
developer-kikikaikai <kikikaikaikai@gmail.com>
2019-02-20 01:19:23 +0900

    Add compiler

--- extra_pkg.sh
+++ extra_pkg.sh
@@ -5,5 +5,5 @@ yum install -y golang python36 sl
 #install DB server
 #curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 yum --disablerepo=mariadb-main install mariadb-server
-
+yum install -y gcc gcc-c++ cmake 
 sl
